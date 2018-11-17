33bf6fa3c9f21758f026922fc97161fc14fcf71c
Paul Sobocinski <paul@humi.ca>
2018-11-17 11:26:40 -0500

    Cells w / board

--- game_of_life.js
+++ game_of_life.js
@@ -1,4 +1,30 @@
-function sum(a, b) {
-  return a + b;
+function Cell(isAlive, neighbours) {
+  this.isAlive = isAlive;
+  this.neighbours = neighbours;
+  this.nextGen = function() {
+    var aliveCount = 0;
+    this.neighbours.forEach(function(neighbour) {
+      if (neighbour.isAlive)
+        aliveCount++;
+    });
+
+    if (this.isAlive == false && aliveCount == 3) {
+      // cell is dead, and we revive it
+      this.isAlive = true;
+    } else if (this.isAlive == false && aliveCount != 3) {
+      // cell is dead, and it stays dead
+      this.isAlive = false;
+    } else {
+      // cell is alive, and it might die
+      this.isAlive = (aliveCount >= 2 && aliveCount < 4);
+    }
+
+    return this;
+  };
 }
-module.exports = sum;
+
+function Board() {
+
+}
+
+module.exports = [Cell, Board];
--- game_of_life.test.js
+++ game_of_life.test.js
@@ -1,5 +1,41 @@
-const sum = require('./game_of_life');
+const [Cell, Board] = require('./game_of_life');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('Cell is dead when has one alive neighbor', () => {
+  var cell = new Cell(true, [ new Cell(false) ]);
+  expect(cell.nextGen().isAlive).toBe(false);
 });
+
+test('Cell is alive when has two alive neighbors', () => {
+  var cell = new Cell(true, [ new Cell(true), new Cell(true) ]);
+  expect(cell.nextGen().isAlive).toBe(true);
+});
+
+test('Cell is alive when has three alive neighbors', () => {
+  var cell = new Cell(true, [ new Cell(true), new Cell(true), new Cell(true) ]);
+  expect(cell.nextGen().isAlive).toBe(true);
+});
+
+test('Cell is dead when has four alive neighbors', () => {
+  var cell = new Cell(true, [ new Cell(true), new Cell(true), new Cell(true), new Cell(true) ]);
+  expect(cell.nextGen().isAlive).toBe(false);
+});
+
+test('Dead cell is alive when has three alive neighbors', () => {
+  var cell = new Cell(false, [ new Cell(true), new Cell(true), new Cell(true) ]);
+  expect(cell.nextGen().isAlive).toBe(true);
+});
+
+test('Dead cell is dead when has two alive neighbors', () => {
+  var cell = new Cell(false, [ new Cell(true), new Cell(true) ]);
+  expect(cell.nextGen().isAlive).toBe(false);
+});
+
+test('Board nextGen kills cells', () => {
+  var cell1 = new Cell(true, [ new Cell(false) ]);
+  var cell2 = new Cell(true, [ new Cell(false) ]);
+  var board = new  Board([cell1, cell2]);
+
+  expect(cell1.neighhours).toBe([cell2])
+});
+
+
